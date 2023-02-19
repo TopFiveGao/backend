793dc410fb47e0c00f0e36e66e9fccbe1af0e6dd
magicBook <2045778136@qq.com>
2023-02-19 16:46:16 +0800

    测试dva基本状态数据功能

--- src/pages/other/index.tsx
+++ src/pages/other/index.tsx
@@ -2,16 +2,27 @@
  * @Author       : topfivegao
  * @Date         : 2022-12-12 22:42:24
  * @FilePath     : /backend/src/pages/other/index.tsx
- * @LastEditTime : 2022-12-12 22:42:25
+ * @LastEditTime : 2023-02-19 15:14:31
  * @Description  : 有空一起吃个饭啊!	微信联系 treeshaking666
  * 
  * Copyright (c) 2022 by topfivegao, All Rights Reserved. 
  */
 
-const Other = () => {
+import React from 'react'
+import { connect } from 'umi'
+import { Divider } from 'antd'
+
+const Other: React.FC = (props: any) => {
+  console.log(props)
   return (
-    <div>其他信息</div>
+    <>
+      <Divider orientation={'left'}>页面 1</Divider>
+      {props.state}
+    </>
   )
 }
 
-export default Other
+export default connect(({ test }: { test: any }) => {
+  console.log('page 1:', test);
+  return { state: test }
+})(Other)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@
  * @Author       : topfivegao
  * @Date         : 2022-12-11 00:58:09
  * @FilePath     : /backend/src/routes/index.ts
- * @LastEditTime : 2023-02-13 12:54:04
+ * @LastEditTime : 2023-02-19 02:46:05
  * @Description  : 有空一起吃个饭啊!	微信联系 treeshaking666
  * 
  * Copyright (c) 2022 by topfivegao, All Rights Reserved. 
@@ -116,8 +116,19 @@ export const routes = [
     },
     {
         path: '/other',
-        component: '@/pages/other',
         name: '其他',
         icon: 'Tool',
+        routes:[
+            {
+                path: '/other/home',
+                component: '@/pages/other',
+                name: '选项一'
+            },
+            {
+                path: '/other/option',
+                component: '@/pages/other/option',
+                name: '选项二'
+            },
+        ]
     }
 ]
